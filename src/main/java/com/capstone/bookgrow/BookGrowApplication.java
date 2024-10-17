306437125d81c36c06a48252eedaf96fe60dbb11
yoondahn <ydmhy1@naver.com>
2024-10-17 14:43:58 +0900

    TASK #1 : 기존 서버 프로젝트 연결

--- src/main/java/com/capstone/bookgrow/BookGrowApplication.java
+++ src/main/java/com/capstone/bookgrow/BookGrowApplication.java
@@ -3,7 +3,7 @@ package com.capstone.bookgrow;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// Git connection test code
+// git connection test code
 @SpringBootApplication
 public class BookGrowApplication  {
 
